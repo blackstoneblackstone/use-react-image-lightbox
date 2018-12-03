489bbdcda2511eb1da1af13efe49defda915ac1f
yondu <yongdu.lhb@alibaba-inc.com>
2018-12-04 00:56:06 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css' // This only needs to be imported once in your app
 
-class App extends Component {
-  render() {
+const images = [
+  '//placekitten.com/1500/500',
+  '//placekitten.com/4000/3000',
+  '//placekitten.com/800/1200',
+  '//placekitten.com/1500/1500'
+]
+
+export default class LightboxExample extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      photoIndex: 0,
+      isOpen: false
+    }
+  }
+
+  render () {
+    const { photoIndex, isOpen } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div style={{ height: '100px', width: '400px', background: '#ccc' }}>
+
+        </div>
+        <div tabIndex="-1" style={{ height: '1000px', width: '400px', background: '#ddd' }}
+             onClick={() => this.setState({ isOpen: true })}>
+          Open Lightbox
+        </div>
+
+        {isOpen && (
+          <Lightbox
+            // 该prop解决关闭弹窗后下层元素滑动
+            reactModalProps={{ shouldReturnFocusAfterClose: false }}
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1) % images.length]}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + images.length - 1) % images.length
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % images.length
+              })
+            }
+          />
+        )}
       </div>
-    );
+    )
   }
 }
-
-export default App;
